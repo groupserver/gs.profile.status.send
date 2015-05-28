6d1aa4faf326e95984d9b9c36c01adc9b4372f1c
Michael JasonSmith <mpj17@onlinegroups.net>
2015-05-28 15:49:42 -0700

    Lack unit tests

--- setup.py
+++ setup.py
@@ -71,8 +71,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
-    tests_require=['mock', ],
-    test_suite="gs.profile.status.send.tests.test_all",
     extras_require={'docs': ['Sphinx'], },
     entry_points={
         'console_scripts': [
